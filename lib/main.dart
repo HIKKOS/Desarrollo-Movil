b517195c6c9d729a748ee4047ef0ebf4f4de2264
Hikkos <noeparedes027@gmail.com>
2023-01-16 10:20:00 -0600

    navigation-basics

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:dummy_app/pages/PageOne.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
