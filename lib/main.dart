42ff4247417c57d954d8e93c3b1c2266c8af204d
Hikkos <noeparedes027@gmail.com>
2023-01-16 11:43:29 -0600

    Navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:dummy_app/pages/PageOne.dart';
+import 'package:dummy_app/pages/PageTwo.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     title: 'Navigation Basics',
-    home: FirstRoute(),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const FirstRoute(),
+      '/second': (context) => const SecondRoute(),
+    },
   ));
 }
